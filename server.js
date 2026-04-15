d2d7855a553c0192c9839931676273a0c15942c4
Lokesh-Chowdary-7 <lokeshsrisai2007@gmail.com>
2026-04-15 20:37:04 +0530

    updated server config for render deployment

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 });
 
 // Connect to MongoDB
-mongoose.connect('process.env.MONGO_URI')
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected successfully'))
     .catch(err => console.error('MongoDB connection error:', err));
 
